01a6f4180c737198df275662491ff1bc2c912e23
NIKHIL KUMAR <108527011+10nikhil01@users.noreply.github.com>
2023-08-26 20:47:50 +0530

    Update LandingPage.js

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -2,9 +2,9 @@ import {React, useEffect} from 'react'
 import Users from '../components/Users'
 import "./LandingPage.css"
 function LandingPage() {
-  // useEffect(() =>{
-  //   localStorage.clear();
-  // })
+  useEffect(() =>{
+    localStorage.clear();
+  })
   return (
     <div className='landingPage'>
         <Users />
