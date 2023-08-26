b12461cee154eaee7e7fb8232ce09981d9c8e234
NIKHIL KUMAR <108527011+10nikhil01@users.noreply.github.com>
2023-08-26 20:48:26 +0530

    Update ProfilePage.js

--- src/Pages/ProfilePage.js
+++ src/Pages/ProfilePage.js
@@ -1,41 +1,23 @@
-import React from "react";
-import {
-  Box,
-  Drawer,
-  Divider,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemText,
-  Grid,
-} from "@mui/material";
+import { React } from "react";
+import { Box, Grid } from "@mui/material";
 import AppBarComp from "../components/AppBarComp";
 import MainInfoComp from "../components/MainInfoComp";
-import { Link } from "react-router-dom";
 import SideBarComp from "../components/SideBarComp";
 import FooterComp from "../components/FooterComp";
 
-
 function ProfilePage() {
   return (
-    // <Box sx={{ display: "flex" }}>
-    //   <Box>
-    //     <AppBarComp />
-    //   </Box>
-    //   <SideBarComp />
-    // </Box>
     <Box>
-    <Grid container sx={{height:"100vh"}}>
-      <Grid item md={2.5} alignItems="center">
-        <SideBarComp />
-      </Grid>
-      <Grid item md={9.5}>
+      <Grid container sx={{ height: "100vh" }}>
+        <Grid item md={2.5} alignItems="center">
+          <SideBarComp />
+        </Grid>
+        <Grid item md={9.5}>
           <AppBarComp />
-        <MainInfoComp />
-        
+          <MainInfoComp />
+        </Grid>
+        <FooterComp />
       </Grid>
-      <FooterComp />
-    </Grid>
     </Box>
   );
 }
