b01fa832f3a5376e70d710707792b4539e68c2bc
NIKHIL KUMAR <108527011+10nikhil01@users.noreply.github.com>
2023-08-26 20:50:08 +0530

    Update MainInfoComp.js

--- src/components/MainInfoComp.js
+++ src/components/MainInfoComp.js
@@ -3,22 +3,23 @@ import { React, useState, useEffect } from "react";
 import Map from "../Assets/Map.png";
 const width = "330px";
 function MainInfoComp() {
-  const [loggedInUser, setLoggedInUser] = useState({});
+  const [selectedUserData, setSelectedUserData] = useState({});
 
   useEffect(() => {
-    const loggedInUser = localStorage.getItem("selectedUserData");
-    if (loggedInUser) {
-      setLoggedInUser(JSON.parse(loggedInUser));
+    const storedSelectedUserData = localStorage.getItem("selectedUserData");
+    if (storedSelectedUserData) {
+      setSelectedUserData(JSON.parse(storedSelectedUserData));
     }
   }, []);
+
   return (
     <Box sx={{}}>
       <Grid container sx={{ width: `calc(100% - ${width})`, ml: 16, mt: 18 }}>
-        <Grid item md={5} sx={{ borderRight: "1px solid #e5e5e5", pr: 10 }}>
+        <Grid item md={5} sx={{ borderRight: "1px solid #e5e5e5", pr: 12 }}>
           <Box sx={{}}>
             <Avatar
               sx={{ width: 200, height: 200, ml: 10 }}
-              src={loggedInUser.profilepicture}
+              src={selectedUserData.profilepicture}
             />
             <Typography
               sx={{
@@ -29,10 +30,10 @@ function MainInfoComp() {
                 my: 2,
               }}
             >
-              {loggedInUser.name}
+              {selectedUserData.name}
             </Typography>
           </Box>
-          <Grid container md={12} sx={{ mb: 1 }}>
+          <Grid container sx={{ mb: 1,}}>
             <Grid
               item
               md={6}
@@ -52,10 +53,10 @@ function MainInfoComp() {
               }}
             >
               {" "}
-              {loggedInUser.name}
+              {selectedUserData.name}
             </Grid>
           </Grid>
-          <Grid container md={12} sx={{ mb: 1 }}>
+          <Grid container sx={{ mb: 1 }}>
             <Grid
               item
               md={6}
@@ -75,10 +76,10 @@ function MainInfoComp() {
               }}
             >
               {" "}
-              {loggedInUser.email}
+              {selectedUserData.email}
             </Grid>
           </Grid>
-          <Grid container md={12} sx={{ mb: 1 }}>
+          <Grid container sx={{ mb: 1 }}>
             <Grid
               item
               md={6}
@@ -98,10 +99,10 @@ function MainInfoComp() {
               }}
             >
               {" "}
-              {loggedInUser.phone}
+              {selectedUserData.phone}
             </Grid>
           </Grid>
-          <Grid container md={12} sx={{ mb: 1 }}>
+          <Grid container sx={{ mb: 1 }}>
             <Grid
               item
               md={6}
@@ -121,18 +122,23 @@ function MainInfoComp() {
               }}
             >
               {" "}
-              {loggedInUser.website}
+              {selectedUserData.website}
             </Grid>
           </Grid>
           <Divider sx={{ my: 2, ml: 6 }} />
           <Grid
             item
             md={12}
-            sx={{ textAlign: "center", fontSize: "1.2rem", mb:1, color: "#999" }}
+            sx={{
+              textAlign: "center",
+              mb: 2,
+              fontSize: "1.2rem",
+              color: "#999",
+            }}
           >
             Company{" "}
           </Grid>
-          <Grid container md={12} sx={{ mb: 1 }}>
+          {/* <Grid container sx={{ mb: 1 }}>
             <Grid
               item
               md={6}
@@ -152,10 +158,10 @@ function MainInfoComp() {
               }}
             >
               {" "}
-              {loggedInUser.company.name}
+              {selectedUserData.company.name}
             </Grid>
           </Grid>
-          <Grid container md={12} sx={{ mb: 1 }}>
+          <Grid container sx={{ mb: 1 }}>
             <Grid
               item
               md={6}
@@ -175,10 +181,10 @@ function MainInfoComp() {
               }}
             >
               {" "}
-              {loggedInUser.company.catchPhrase}
+              {selectedUserData.company.catchPhrase}
             </Grid>
           </Grid>
-          <Grid container md={12} sx={{ mb: 1 }}>
+          <Grid container sx={{ mb: 1 }}>
             <Grid
               item
               md={6}
@@ -198,14 +204,144 @@ function MainInfoComp() {
               }}
             >
               {" "}
-              {loggedInUser.company.bs}
+              {selectedUserData.company.bs}
             </Grid>
-          </Grid>
+          </Grid> */}
         </Grid>
         <Grid item md={7} sx={{ pl: 6 }}>
+          <Grid
+            item
+            md={12}
+            sx={{ textAlign: "left", fontSize: "1.2rem", mb: 2, color: "#999" }}
+          >
+            Address{" :"}{" "}
+          </Grid>
+          {/* <Grid container sx={{ mb: 1 }}>
+            <Grid
+              item
+              md={2}
+              sx={{ textAlign: "right", fontSize: "1.2rem", color: "#999" }}
+            >
+              Street{" :"}{" "}
+            </Grid>
+            <Grid
+              item
+              md={6}
+              sx={{
+                textAlign: "left",
+                pl: 1,
+                fontSize: "1.2rem",
+                fontWeight: "600",
+                color: "#555",
+              }}
+            >
+              {" "}
+              {selectedUserData.address.street}
+            </Grid>
+          </Grid>
+          <Grid container sx={{ mb: 1 }}>
+            <Grid
+              item
+              md={2}
+              sx={{ textAlign: "right", fontSize: "1.2rem", color: "#999" }}
+            >
+              Suite{" :"}{" "}
+            </Grid>
+            <Grid
+              item
+              md={6}
+              sx={{
+                textAlign: "left",
+                pl: 1,
+                fontSize: "1.2rem",
+                fontWeight: "600",
+                color: "#555",
+              }}
+            >
+              {" "}
+              {selectedUserData.address.suite}
+            </Grid>
+          </Grid>
+          <Grid container sx={{ mb: 1 }}>
+            <Grid
+              item
+              md={2}
+              sx={{ textAlign: "right", fontSize: "1.2rem", color: "#999" }}
+            >
+              City{" :"}{" "}
+            </Grid>
+            <Grid
+              item
+              md={6}
+              sx={{
+                textAlign: "left",
+                pl: 1,
+                fontSize: "1.2rem",
+                fontWeight: "600",
+                color: "#555",
+              }}
+            >
+              {" "}
+              {selectedUserData.address.city}
+            </Grid>
+          </Grid>
+          <Grid container sx={{ mb: 2 }}>
+            <Grid
+              item
+              md={2}
+              sx={{ textAlign: "right", fontSize: "1.2rem", color: "#999" }}
+            >
+              Zipcode{" :"}{" "}
+            </Grid>
+            <Grid
+              item
+              md={6}
+              sx={{
+                textAlign: "left",
+                pl: 1,
+                fontSize: "1.2rem",
+                fontWeight: "600",
+                color: "#555",
+              }}
+            >
+              {" "}
+              {selectedUserData.address.zipcode}
+            </Grid>
+          </Grid> */}
           <Box>
-            <img src={Map} />
+            <img
+              style={{ maxWidth: "35rem", borderRadius: "30px" }}
+              src={Map}
+              alt="map"
+            />
           </Box>
+          {/* <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-end",
+              gap: 2,
+              width: "100%",
+              mt: 1,
+            }}
+          >
+            <Typography sx={{ fontSize: "1rem", color: "#999" }}>
+              Lat:{" "}
+              <span
+                style={{ color: "#000", fontSize: "1.1rem", fontWeight: "600" }}
+              >
+                {selectedUserData.address.geo.lat}
+              </span>
+            </Typography>
+            <Typography sx={{ fontSize: "1rem", color: "#999" }}>
+              Long:{" "}
+              <span
+                style={{ color: "#000", fontSize: "1.1rem", fontWeight: "600" }}
+              >
+                {selectedUserData.address.geo.lng}
+              </span>
+            </Typography>
+          </Box> */}
         </Grid>
       </Grid>
     </Box>
